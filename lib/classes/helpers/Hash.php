c1e60c42ee5df8a10e962f1377b4321e0e961378
Sebastian Book <sebbebook@gmail.com>
2011-10-13 11:01:48 +0200

    changed class name to hash

--- lib/classes/helpers/Hash.php
+++ lib/classes/helpers/Hash.php
@@ -18,7 +18,7 @@
  * @copyright   Copyright (c) 2011 Sebastian Book <sebbebook@gmail.com>
  * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-class Security extends Factory
+class Hash extends Factory
 {
     /**
      * Constructor
