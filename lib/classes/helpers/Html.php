13bd82542eda140546827953e5e19f2714a4f9f5
Sebastian Book <sebbebook@gmail.com>
2011-10-14 22:08:48 +0200

    placeholder classes to be implemented

---
+++ lib/classes/helpers/Html.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * CloudLib :: Lightweight MVC PHP Framework
+ *
+ * @author      Sebastian Book <sebbebook@gmail.com>
+ * @copyright   Copyright (c) 2011 Sebastian Book <sebbebook@gmail.com>
+ * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @package     cloudlib
+ */
+
+/**
+ * The html class.
+ *
+ * <short description>
+ *
+ * Pointers on what could be implemented:
+ *
+ * CSS (<link>)
+ * SCRIPT (<script>)
+ * META (favicon/charset etc..)
+ * LINK (<a>)
+ * IMAGE (<img>)
+ * DOCTYPE (HTML5)
+ * CSS / SCRIPT codeblocks?
+ * DIV (<div>)
+ * PARAGRAPH (<p>)
+ * TABLES (<table>)
+ * HEADERS (<h1-6>)
+ *
+ * Default <head> block?, array of options?
+ *
+ * @package     cloudlib
+ * @subpackage  cloudlib.lib.classes
+ * @copyright   Copyright (c) 2011 Sebastian Book <sebbebook@gmail.com>
+ * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+final class Html extends Factory
+{
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @return  void
+     */
+    public function __construct() {}
+
+    public function css()
+    {
+
+    }
+
+    public function script()
+    {
+
+    }
+
+    public function link()
+    {
+
+    }
+
+    public function img()
+    {
+
+    }
+}
