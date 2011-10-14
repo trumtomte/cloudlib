e500fcb94159878c0f9861e58ea0fa630bc37537
Sebastian Book <sebbebook@gmail.com>
2011-10-14 18:35:25 +0200

    print more info, testing

--- app/views/index.php
+++ app/views/index.php
@@ -5,5 +5,10 @@
     echo '<pre>';
     debug_print_backtrace();
     echo '</pre>';
+    echo '<pre>';
+    echo print_r($_SERVER);
+    echo '<br>';
+    echo print_r($_REQUEST);
+    echo '</pre>';
 
 ?>
