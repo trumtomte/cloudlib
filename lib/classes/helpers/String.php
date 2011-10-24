14b22d5a9f800c0a9ea1999d55cd3191ae2e9d3b
Sebastian Book <sebbebook@gmail.com>
2011-10-24 18:15:18 +0200

    removed unnecessary functions

--- lib/classes/helpers/String.php
+++ lib/classes/helpers/String.php
@@ -14,7 +14,7 @@
  * <short description>
  *
  * @package     cloudlib
- * @subpackage  cloudlib.lib.classes
+ * @subpackage  cloudlib.lib.classes.helpers
  * @copyright   Copyright (c) 2011 Sebastian Book <sebbebook@gmail.com>
  * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
@@ -29,83 +29,6 @@ class String extends Factory
     public function __construct() {}
 
     /**
-     * Shorthand for mb_strtoupper
-     *
-     * @access  public
-     * @return  void
-     */
-    public static function upper($string)
-    {
-        return mb_strtoupper($string);
-    }
-
-    /**
-     * Shorthand for mb_strtolower
-     *
-     * @access  public
-     * @return  void
-     */
-    public static function lower($string)
-    {
-        return mb_strtolower($string);
-    }
-
-    /**
-     * Shorthand for mb_ereg_replace
-     *
-     * @access  public
-     * @return  void
-     */
-    public static function replace($pattern, $replacement, $string, $options = null)
-    {
-        return mb_ereg_replace($pattern, $replacement, $string, $options);
-    }
-
-    /**
-     * Shorthand for mb_ereg_match
-     *
-     * @access  public
-     * @return  void
-     */
-    public static function match($pattern, $string, $options = null)
-    {
-        return mb_ereg_match($pattern, $string, $options);
-    }
-
-    /**
-     * Shorthand for mb_strlen
-     *
-     * @access  public
-     * @return  void
-     */
-    public static function length($string)
-    {
-        return mb_strlen($string);
-    }
-
-    /**
-     * Shorthand for mb_strimwidth
-     *
-     * @access  public
-     * @return  void
-     */
-    public static function trimwidth($string, $start, $width, $marker = null)
-    {
-        return mb_strimwidth($string, $start, $width, $marker);
-    }
-
-    /**
-     * Shorthand for mb_substr
-     *
-     * @access  public
-     * @return  void
-     */
-    public static function sub($string, $start, $length = null)
-    {
-        return mb_substr($string, $start, $length);
-    }
-
-    /**
      * Repeat a string 
      *
      * @access  public
@@ -116,9 +39,9 @@ class String extends Factory
      */
     public static function repeat($string, $times = 2, $separator = null)
     {
-        if(isset($separator))
+        if($separator !== null)
         {
-            return str_repeat($string . $separator, ($times - 1)) . $separator;
+            return str_repeat($string . $separator, ($times - 1)) . $string;
         }
 
         return str_repeat($string, $times);
