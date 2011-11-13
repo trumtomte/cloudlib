eeab67f3dd1e68dcbf860c878b9442975f69a870
Sebastian Book <sebbebook@gmail.com>
2011-11-13 20:36:56 +0100

    moved from Empty directory to Helpers directory

---
+++ lib/classes/helpers/Paginator.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * CloudLib :: Lightweight MVC PHP Framework
+ *
+ * @author      Sebastian Book <sebbebook@gmail.com>
+ * @copyright   Copyright (c) 2011 Sebastian Book <sebbebook@gmail.com>
+ * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @package     cloudlib
+ */
+
+/**
+ * The paginator class.
+ *
+ * <short description>
+ *
+ * @package     cloudlib
+ * @subpackage  cloudlib.lib.classes.helpers
+ * @copyright   Copyright (c) 2011 Sebastian Book <sebbebook@gmail.com>
+ * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+class Paginator extends Factory
+{
+    // TBD
+    // Class for pagination
+}
---
+++ lib/classes/helpers/Validator.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * CloudLib :: Lightweight MVC PHP Framework
+ *
+ * @author      Sebastian Book <sebbebook@gmail.com>
+ * @copyright   Copyright (c) 2011 Sebastian Book <sebbebook@gmail.com>
+ * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @package     cloudlib
+ */
+
+/**
+ * The validator class.
+ *
+ * <short description>
+ *
+ * @package     cloudlib
+ * @subpackage  cloudlib.lib.classes.helpers
+ * @copyright   Copyright (c) 2011 Sebastian Book <sebbebook@gmail.com>
+ * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+class Validator extends Factory
+{
+    // TBD
+    // Class for validating data/input
+}
