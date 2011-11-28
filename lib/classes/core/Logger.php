dc4d592c4a68133a101fd5fab9fa0ab14666bbe3
Sebastian Book <sebbebook@gmail.com>
2011-11-28 17:56:13 +0100

    removed parent class

--- lib/classes/core/Logger.php
+++ lib/classes/core/Logger.php
@@ -18,7 +18,7 @@
  * @copyright   Copyright (c) 2011 Sebastian Book <sebbebook@gmail.com>
  * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-final class Logger extends Factory
+class Logger
 {
     /**
      * Constant for information messages
