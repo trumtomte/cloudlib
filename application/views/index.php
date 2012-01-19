7cf7894ff0a43b736a792d0ddb9b19079ed924b1
Sebastian Book <sebbebook@gmail.com>
2012-01-20 00:07:37 +0100

    ?

--- application/views/index.php
+++ application/views/index.php
@@ -1,18 +1,10 @@
 <?php
-    echo Benchmark::compare(BOOTTIME);
-    echo '<br>';
     echo '<pre>';
     debug_print_backtrace();
-    echo '</pre>';
-    echo '<pre>';
+    echo '</pre><pre>';
     echo var_dump($_SERVER);
-    echo '</pre>';
-    echo '<pre>';
-    if(isset($test))
-    {
-    echo var_dump($test);
+    echo '</pre><pre>Test variable: <hr>';
+    if(isset($test)) {
+    echo print_r($test);
     }
-    echo '</pre>';
-    echo '<br>';
-    echo Benchmark::compare(BOOTTIME);
-
+    echo '</pre><hr>';
