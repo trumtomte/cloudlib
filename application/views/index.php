37ad41bff1ad8a0a09a00b87a95504cc1e80695b
Sebastian Book <sebbebook@gmail.com>
2012-03-28 00:16:58 +0200

    Deleted unused files

--- application/views/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-    echo 'Welcome to the startpage!<br />';
-    echo 'This file is located in application/views/index.php,<br />';
-    echo 'this is a view which uses the layout index.php in application/views/layouts/index.php<br />';
