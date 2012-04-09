3c820afef21a79c6cc7ab5125e792b8e6dffab4f
Sebastian Book <sebbebook@gmail.com>
2012-04-09 21:25:02 +0200

    only remove first occurrence of the base uri

--- cloudlib/Router.php
+++ cloudlib/Router.php
@@ -132,7 +132,7 @@ class Router
         $method = ($app->request->isHead()) ? 'GET' : $app->request->method;
 
         // Strip the base uri from the requested uri
-        $request = '/' . ltrim(preg_replace('#' . $app->base . '#', '', $app->request->uri), '/');
+        $request = '/' . ltrim(preg_replace('#' . $app->base . '#', '', $app->request->uri, 1), '/');
 
         foreach($this->routes as $route => $responses)
         {
