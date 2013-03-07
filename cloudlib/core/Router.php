a91140a4c8cfd9f03d354c3adc1d45dd30edab74
Sebastian Book <sebbebook@gmail.com>
2013-03-07 23:08:56 +0100

    remade to be more simple and abstract

--- cloudlib/core/Router.php
+++ cloudlib/core/Router.php
@@ -9,7 +9,6 @@
 
 namespace cloudlib\core;
 
-use cloudlib\core\Cloudlib;
 use cloudlib\core\Route;
 
 /**
@@ -34,7 +33,7 @@ class Router
      * @access  public
      * @var     array
      */
-    public $routes = array();
+    public $routes = [];
 
     /**
      * Array of matching routes for the current request
@@ -53,125 +52,39 @@ class Router
      * @param   mixed   $response   The route response
      * @return  object              The newly added route
      */
-    public function add($route, $method, $response)
+    public function add($route, callable $response)
     {
-        // Sets the route and then returns it
-        return $this->routes[sprintf('%s %s', $method, $route)] = new Route($route, $method, $response);
-    }
+        $uri = $route;
+        $methods = ['GET'];
 
-    /**
-     * Add multiple (or a single one) routes
-     *
-     * @access  public
-     * @param   string  $route      The route uri
-     * @param   array   $methods    Array of allowed request method
-     * @param   mixed   $response   The route response
-     * @return  void
-     */
-    public function route($route, array $methods, $response)
-    {
-        foreach($methods as $method)
+        if($route[0] !== '/')
         {
-            $this->add($route, $method, $response);
+            list($methods, $uri) = explode(' ', $route);
+            $methods = explode('|', $methods);
+        }
+
+        if(isset($this->routes[$uri]))
+        {
+            $this->routes[$uri]->append($methods, $response);
+        }
+        else
+        {
+            $this->routes[$uri] = new Route($uri, $methods, $response);
         }
     }
 
-    /**
-     * Shorthand function for adding a route which allows the GET method
-     *
-     * @access  public
-     * @param   string  $route      The route uri
-     * @param   mixed   $response   The route response
-     * @return  object              The newly added route
-     */
-    public function get($route, $response)
+    public function find($request)
     {
-        return $this->add($route, 'GET', $response);
-    }
-
-    /**
-     * Shorthand function for adding a route which allows the POST method
-     *
-     * @access  public
-     * @param   string  $route      The route uri
-     * @param   mixed   $response   The route response
-     * @return  object              The newly added route
-     */
-    public function post($route, $response)
-    {
-        return $this->add($route, 'POST', $response);
-    }
-
-    /**
-     * Shorthand function for adding a route which allows the PUT method
-     *
-     * @access  public
-     * @param   string  $route      The route uri
-     * @param   mixed   $response   The route response
-     * @return  object              The newly added route
-     */
-    public function put($route, $response)
-    {
-        return $this->add($route, 'PUT', $response);
-    }
-
-    /**
-     * Shorthand function for adding a route which allows the DELETE method
-     *
-     * @access  public
-     * @param   string  $route      The route uri
-     * @param   mixed   $response   The route response
-     * @return  object              The newly added route
-     */
-    public function delete($route, $response)
-    {
-        return $this->add($route, 'DELETE', $response);
-    }
-
-    /**
-     * Check if a route exists based on $uri and $base
-     *
-     * @access  public
-     * @param   string  $base   The base uri
-     * @param   string  $uri    The requested uri
-     * @return  boolean         Returns true if a matching route does exist
-     */
-    public function routeExists($base, $uri)
-    {
-        $request = preg_replace('/\/{2,}/', '/', '/' . preg_replace('#' . $base . '#', '', $uri, 1));
+        $match = null;
 
         foreach($this->routes as $route)
         {
             if($route->match($request))
             {
-                $route->setRequest($request);
-
-                $this->matchingRoutes[] = $route;
+                $match = $route;
             }
         }
 
-        return empty($this->matchingRoutes) ? false : true;
-    }
-
-    /**
-     * Check if the requested route allows the request $method
-     *
-     * @access  public
-     * @param   string  $method
-     * @return  boolean Returns true if the route allows the requested method
-     */
-    public function routeHasMethod($method)
-    {
-        $method = ($method == 'HEAD') ? 'GET' : $method;
-
-        foreach($this->matchingRoutes as $route)
-        {
-            if($route->hasMethod($method))
-            {
-                $this->route = $route;
-            }
-        }
-
-        return isset($this->route) ? true : false;
+        return $match;
     }
 }
